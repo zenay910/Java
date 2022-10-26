045e6404e671b0ab031d8479a600cfbc64d23e7b
ism178 <ism178@ensign.edu>
2022-10-26 15:05:32 -0700

    ss

--- src/WEEK01/Lab1a.java
+++ src/WEEK01/Lab1a.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Lab1a {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("Error: Could not find or load main class WEEK06.Lab6e in module CS105");
 
 	}
 }

b6e70c3f1c93c8a66d3ec6eb3d03e8a639679101
ism178 <ism178@ensign.edu>
2022-11-03 07:41:24 -0700

    dsds

---
+++ src/WEEK08/Lab8a.java
@@ -0,0 +1,20 @@
+package WEEK08;
+import java.util.*;
+public class Lab8a {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+
+
+      
+
+
+
+
+
+
+
+
+
+
+      }
+}

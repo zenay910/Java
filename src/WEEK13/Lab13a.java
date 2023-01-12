9c24167e66213f28a406d4d274aad3a47870dad9
ism178 <ism178@ensign.edu>
2023-01-12 12:56:16 -0700

    kk

---
+++ src/WEEK13/Lab13a.java
@@ -0,0 +1,129 @@
+package WEEK13;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class Lab13a {
+
+
+	public static void main(String[] args) {
+		// main goes here
+		// Variables
+		File file = new File("Pwd.txt");
+		Scanner pf = null; // initialize scanner
+		PrintWriter valid = null;
+		PrintWriter invalid = null;
+		String validStr = "validpwd.txt";
+		String invalidStr = "invalidpwd.txt";
+		String passwd = "";
+		boolean done = false;
+
+		while (!done) {
+			// check if pwd.txt is found
+			try {
+				pf = new Scanner(file);
+			} catch (Exception e) {
+				System.out.println("Error. We cannot find this file.");
+				break;
+			} // end of try/catch
+
+			// Create valid file
+			try {
+				valid = new PrintWriter(validStr);
+			} catch (FileNotFoundException e) {
+				System.out.println("Error! Unable to create: " + validStr);
+			}
+
+			// Create invalid file
+			try {
+				invalid = new PrintWriter(invalidStr);
+			} catch (FileNotFoundException e) {
+				System.out.println("Error! Unable to create: " + invalidStr);
+			}
+
+			// While loop to go through each word in Pwd.txt file
+			while (pf.hasNext()) {
+				passwd = pf.nextLine();
+				// if statements
+				if (!(passwd.length() >= 8)) {
+					invalid.printf("%-15s Invalid Password! Must be at least 8 characters long.%n", passwd);
+				} else if (!hasUpper(passwd)) {
+					invalid.printf("%-15s Invalid Password! Must contain at least one UPPERCASE character.%n", passwd);
+				} else if (!hasLower(passwd)) {
+					invalid.printf("%-15s Invalid Password! Must contain at least one LOWERCASE character.%n", passwd);
+				} else if (!hasNumber(passwd)) {
+					invalid.printf("%-15s Invalid Password! Must contain at least one NUMBER.%n", passwd);
+				} else if (!hasSpecial(passwd)) {
+					invalid.printf("%-15s Invalid Password! Must contain at least one of {@,#,%s,-,&,*}.%n", passwd, "%");
+				}else {
+					valid.printf("%s%n", passwd);
+				}
+
+			} // end of while
+			//close files
+			invalid.close();
+			valid.close();
+			
+
+			done = true;
+		} // end of main while
+
+	} // end of main()
+
+	public static boolean hasUpper(String passwd) {
+		// for loop to check every character in password
+		for (int i = 0; i < passwd.length(); i++) {
+			if (Character.isUpperCase(passwd.charAt(i))) {
+				return true;
+			}
+		} // end of for
+
+		return false;
+
+	} // end of hasUpper
+
+	public static boolean hasLower(String passwd) {
+		// for loop to check every character in password
+		for (int i = 0; i < passwd.length(); i++) {
+			if (Character.isLowerCase(passwd.charAt(i))) {
+				return true;
+			}
+		} // end of for
+
+		return false;
+
+	} // end of hasLower
+
+	public static boolean hasNumber(String passwd) {
+		// for loop to check every character in password
+		for (int i = 0; i < passwd.length(); i++) {
+			if (Character.isDigit(passwd.charAt(i))) {
+				return true;
+			}
+		} // end of for
+
+		return false;
+
+	} // end of hasNumber
+
+	public static boolean hasSpecial(String passwd) {
+		// for loop to check every character in password
+		String special = "@#%-&*";
+		for (int i = 0; i < passwd.length(); i++) {
+			//Special character string
+			for (int j = 0; j < special.length(); j++) {
+				// compare each character with special characters
+				if (passwd.charAt(i) == special.charAt(j)) {
+					return true;
+				}
+			} // end of within for loop
+		} // end of for
+
+		return false;
+
+	} // end of hasNumber
+
+}
